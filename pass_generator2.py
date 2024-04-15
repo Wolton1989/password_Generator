4e3cd30fc7cad7f4941b9d72c19827e86d4c00bb
Wolton Dakha <106119598+Wolton1989@users.noreply.github.com>
2024-04-15 16:13:15 +0200

    Update pass_generator2.py

--- pass_generator2.py
+++ pass_generator2.py
@@ -3,7 +3,7 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
+
 
 
 number_of_letters = int(input("How many letters would you like in your password?\n")) 
@@ -13,13 +13,13 @@ number_of_numbers = int(input(f"How many numbers would you like?\n"))
 
 password_list = []
 
-for char in range(1, number_of_letters + 1):
+for charector in range(1, number_of_letters + 1):
   password_list.append(random.choice(letters))
 
-for char in range(1, number_of_symbols + 1):
+for charector in range(1, number_of_symbols + 1):
   password_list += random.choice(symbols)
 
-for char in range(1, number_of_numbers + 1):
+for charector in range(1, number_of_numbers + 1):
   password_list += random.choice(numbers)
 
 
@@ -27,7 +27,7 @@ random.shuffle(password_list)
 print(password_list)
 
 password = ""
-for char in password_list:
+for charector in password_list:
   password += char
 
 print(f"Your password is: {password}")
